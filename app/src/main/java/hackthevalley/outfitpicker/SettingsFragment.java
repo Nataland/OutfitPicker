d5390d648e5218bbd0838f000a61d87285b4b0f7
ThaoTrann <6.28.tran@gmail.com>
2018-02-24 15:58:24 -0500

    file uploaded to firebase

--- app/src/main/java/hackthevalley/outfitpicker/SettingsFragment.java
+++ app/src/main/java/hackthevalley/outfitpicker/SettingsFragment.java
@@ -1,10 +1,12 @@
 package hackthevalley.outfitpicker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by natalie on 2018-02-24.
@@ -14,6 +16,14 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        Button btn = view.findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), FirebaseUploadActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
